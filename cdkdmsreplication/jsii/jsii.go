1477a64c7841560b1e31fc02a32ab0da43868930
kckempf <kckempf@gmail.com>
2026-07-23 04:14:01 +0000

    chore(release): v0.2.19

--- cdkdmsreplication/jsii/jsii.go
+++ cdkdmsreplication/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed cdk-dms-replication-0.2.18.tgz
+//go:embed cdk-dms-replication-0.2.19.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("cdk-dms-replication", "0.2.18", tarball)
+	_jsii_.Load("cdk-dms-replication", "0.2.19", tarball)
 }
